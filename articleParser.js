59c6ebedb02c198e6b5d8ef32e120f33c7991f46
Felipe <philipe.s.xavier@gmail.com>
2019-09-04 16:31:23 -0700

    included flags in puppeteer

--- articleParser.js
+++ articleParser.js
@@ -55,8 +55,10 @@ var articleLinks = [
 ];
 
 async function getText2(url) {
-  console.log("this is the urllllllllllllllllllllllllllllllllllllllll");
-  const browser = await puppeteer.launch();
+  console.log("going within getText2========================");
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
   const page = await browser.newPage();
   await page.goto(url);
 
