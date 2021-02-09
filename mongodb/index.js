7630d38d433f2f0ef35537664511dca0b5fab6a8
Felipe Lima <fsxlima@gmail.com>
2021-02-08 23:27:06 -0500

    removes mongo.config from master

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,14 +1,12 @@
 var mongoose = require("mongoose");
-var devCredentials = require("./mongo.config");
-var credentials = process.env.MONGO_URI ? process.env.MONGO_URI : devCredentials
-
+var credentials = process.env.MONGO_URI;
 
 mongoose
   .connect(credentials, { dbName: "news-analyzer", useNewUrlParser: true })
   .then(() => {
     console.log("connected to mongo db!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("could not connect to mongo", err);
   });
 
@@ -17,5 +15,5 @@ mongoose.set("useFindAndModify", false);
 let db = mongoose.connection;
 
 module.exports = {
-  db
+  db,
 };
